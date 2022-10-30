84b0a24879a2266057a9d67f971dc50b632a58cc
sandy8314 <sandylingayat83@gmail.com>
2022-10-30 13:49:02 +0530

    tesing
    testing

---
+++ eye_disease_detection/lib/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : {super(key: key});
+
+  @override
+  Widget build(BuildContext context) {
+    var ;
+    return Scaffold(
+      body: Container{
+        child : Text(
+          'Splash Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.))
+      },
+    );
+  }
+}
