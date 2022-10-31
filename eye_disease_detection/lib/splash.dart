5cb5ae89444813ba08aaedd0c71140b0c03b8466
kavishasavla12 <kavisha.savla16034@sakec.ac.in>
2022-11-01 01:33:20 +0700

    splash
    splash screen

--- eye_disease_detection/lib/splash.dart
+++ eye_disease_detection/lib/splash.dart
@@ -1,18 +1,44 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 
-class Splash extends StatelessWidget {
-  const Splash({Key? key}) : {super(key: key});
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+   
+   @override
+   _SplashState createState() => _SplashState();
+}
+   
+   class _SplashState extends State<Splash>{
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  } 
+
+  _navigatetohome()async{
+    await Future.delayed(Duration (milliseconds:1500),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> MyHomePage(title: 'Dr.EYE')));
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    var ;
+
     return Scaffold(
-      body: Container{
+      body: Container(
+         Container(height:100, width:100, colors.blue,)
         child : Text(
-          'Splash Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.))
+           'Splash Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
       },
     );
   }
+}
+
+class StatefulWidget {
+}
+
+class StatelessWidget {
 }
